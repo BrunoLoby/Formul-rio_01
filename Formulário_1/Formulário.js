9b6444bdbeb13c3d6ea35242a815a7cc2f89ae5b
Bruno Lobianco <135769838+BrunoLoby@users.noreply.github.com>
2024-08-08 16:07:41 -0300

    Add files via upload

---
+++ Formulário_1/Formulário.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form")
+const nome = document.getElementById("nome")
+const sobrenome = document.getElementById("sobrenome")
+const email = document.getElementById("email")
+
+form.addEventListener("submit", (e) => {
+    event.preventDefault();
+
+    alert("Cadastro realizado com sucesso")
+
+})
+
